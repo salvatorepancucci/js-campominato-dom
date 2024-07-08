96f6d7b1b1bbf74d768cbaf5a73acd841acacc5e
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-08 18:21:39 +0200

    Generated bombs array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 // Seleziona il bottone e il contenitore della griglia dal DOM
 const generateGridButton = document.getElementById('generateGrid');
 const gridContainer = document.getElementById('gridContainer');
+
+let bombe = [];
+let punteggio = 0;
